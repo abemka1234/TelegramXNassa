1a1068d761120e9ccf6aa89274c892ab2e6cc156
abemka1234 <alesparfenov@gmail.com>
2024-10-02 17:52:46 +0300

    Fix of Epic.py

--- Epic.py
+++ Epic.py
@@ -14,31 +14,15 @@ def EPIC(api_key):
 	}
 	response = requests.get('https://api.nasa.gov/EPIC/api/natural', params=params)
 	if response:
-	    for num, epic_image in enumerate(response.json()):
-		        date,time = epic_image["date"].split(" ")
-		        picture = epic_image["image"]
-		      	year,month,day = date.split("-")
-		        print(date)
-
-		        ##strdate = datetime.datetime.fromisoformat(date)
-		        # month = date.month
-		        # day = date.day
-		        # part = (
-		        #     str(date.year)
-		        #     + "/"
-		        #     + month
-		        #     + "/"
-		        #     + day
-		        #     + "/png/"
-		        #     + picture
-		        #     + ".png"
-		        # )
-		        link = "https://api.nasa.gov/EPIC/archive/natural/{0}".format(part)
-		        content = requests.get(link,params=params)
-		        fname = "images/Nasa{0}.png".format(num)
-		        ##with open(fname, "wb") as file:
-		            ##file.write(content.content)
-		        ##fname = ""
+		for num,epic_image in enumerate(response.json()):
+			date,time = epic_image["date"].split(" ")
+			picture = epic_image
+			year,month,day = date.split("-")
+			link = "https://api.nasa.gov/Epic/archive/natural/{0}/{1}/{2}/png/{3}.png".format(year,month,day,picture)
+			content = requests.get(link,params=params)
+			fname = "images/Nasa{0}.png".format(num)
+			with open(fname,"wb") as file:
+				file.write(content.content)
 
 
 if __name__ == "__main__":
