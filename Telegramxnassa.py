0dfbfe8c0b52ddb27f4159f562cacda74406279d
abemka1234 <alesparfenov@gmail.com>
2024-10-09 17:09:36 +0300

    Updated Telegramxnassa

--- Telegramxnassa.py
+++ Telegramxnassa.py
@@ -4,18 +4,20 @@ import os
 import random
 from dotenv import load_dotenv
 import time
-
-load_dotenv()
-upload_frequency= os.getenv("UPL_FRQ")
-TELEGRAM_ID = int(os.getenv("TELEGRAM_ID"))
-Telegram_API = os.getenv("Telegram_API")
-if upload_frequency == None:
-	upload_frequency = 14400
-bot = Bot(token=Telegram_API)
-for root, dirs, files in os.walk('images'):
-	random.shuffle(files)
-	for image_name in files:
-		image_path = os.path.join("images",image_name)
-		with open(image_path,"rb") as f:
-		    bot.send_photo(photo=f,chat_id=TELEGRAM_ID)
-		time.sleep(upload_frequency)
+if __name__ == "__main__":
+	main()
+def main():
+	load_dotenv()
+	upload_frequency= os.getenv("UPL_FRQ")
+	telegram_id = int(os.getenv("TELEGRAM_ID"))
+	telegram_api = os.getenv("Telegram_API")
+	if upload_frequency == None:
+		upload_frequency = 14400
+	bot = Bot(token=telegram_api)
+	for root, dirs, files in os.walk('images'):
+		random.shuffle(files)
+		for image_name in files:
+			image_path = os.path.join("images",image_name)
+			with open(image_path,"rb") as f:
+				bot.send_photo(photo=f,chat_id=telegram_id)
+			time.sleep(upload_frequency)
