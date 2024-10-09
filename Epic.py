339f423799172cce5332d430d940c409007f543a
abemka1234 <alesparfenov@gmail.com>
2024-10-09 17:32:37 +0300

    changed api_key name and used function for downloading pictures in Epic.py

--- Epic.py
+++ Epic.py
@@ -1,14 +1,10 @@
 import requests
 import os
 from dotenv import load_dotenv
+from tools import download_picture
 
 
-def get_expansion(filename):
-    pathparts = os.path.splitext(filename)
-    return pathparts[1]
-
-
-def EPIC(api_key):
+def get_epic_photos(api_key):
 	params = {
 		'api_key': api_key,
 	}
@@ -18,14 +14,13 @@ def EPIC(api_key):
 		date,time = epic_image["date"].split(" ")
 		picture = epic_image
 		year,month,day = date.split("-")
-		link = "https://api.nasa.gov/Epic/archive/natural/{0}/{1}/{2}/png/{3}.png".format(year,month,day,picture)
-		content = requests.get(link,params=params)
-		fname = "images/Nasa{0}.png".format(num)
-		with open(fname,"wb") as file:
-			file.write(content.content)
+		link = "https://api.nasa.gov/Epic/archive/natural/{0}/{1}/{2}/png/{3}".format(year,month,day,picture)
+		fname = "Nasa{0}.png".format(num)
+		download_picture(link,fname,folder_name)
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    API_KEY = os.getenv("NASSA_API_KEY")
-    EPIC(API_KEY)
+	load_dotenv()
+    api_key = os.getenv("NASSA_API_KEY")
+	folder_name = os.getenv("FOLDER_NAME")
+	get_epic_photos(api_key)
