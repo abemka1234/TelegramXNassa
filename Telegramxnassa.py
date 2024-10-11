d3944c1173b873b31ddf96313a6e848088f12dfd
abemka1234 <alesparfenov@gmail.com>
2024-10-11 17:36:10 +0300

    Fixed Telegramxnassa.py

--- Telegramxnassa.py
+++ Telegramxnassa.py
@@ -1,25 +1,27 @@
 from telegram import *
-import requests
 import os
 import random
 from dotenv import load_dotenv
 import time
+
+
+def main():
+	while True:
+		load_dotenv()
+		upload_frequency= os.getenv("UPL_FRQ")
+		telegram_id = int(os.getenv("TELEGRAM_CHAT_ID"))
+		telegram_api = os.getenv("TELEGRAM_API")
+		folder_name = os.getenv("FOLDER_NAME")
+		bot = Bot(token=telegram_api)
+		for root, dirs, files in os.walk(folder_name):
+			random.shuffle(files)
+			for image_name in files:
+				image_path = os.path.join(folder_name,image_name)
+				with open(image_path,"rb") as f:
+					bot.send_photo(photo=f,chat_id=telegram_id)
+				time.sleep(5)
+		time.sleep(upload_frequency)
+
+
 if __name__ == "__main__":
 	main()
-
-	
-def main():
-	load_dotenv()
-	upload_frequency= os.getenv("UPL_FRQ")
-	telegram_id = int(os.getenv("TELEGRAM_ID"))
-	telegram_api = os.getenv("Telegram_API")
-	if upload_frequency == None:
-		upload_frequency = 14400
-	bot = Bot(token=telegram_api)
-	for root, dirs, files in os.walk('images'):
-		random.shuffle(files)
-		for image_name in files:
-			image_path = os.path.join("images",image_name)
-			with open(image_path,"rb") as f:
-				bot.send_photo(photo=f,chat_id=telegram_id)
-			time.sleep(upload_frequency)
